98105b594dc460f8c0c51d0aaa20dd578ffa69cc
Your Name <you@example.com>
2024-09-22 21:35:40 +0000

    FIX : fixing Wordpress container to work with .env

--- srcs/requirements/wordpress/wp_setup.sh
+++ srcs/requirements/wordpress/wp_setup.sh
@@ -11,14 +11,22 @@ mv wp-cli.phar /usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
 
-chmod -R 755 /var/www/html/wp-content
 
 wp core download --path="/var/www/html" --allow-root
 
+wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb
 
+wp config set --allow-root WP_REDIS_HOST 'redis'
+wp config set --allow-root WP_REDIS_PORT 6379
+wp config set --allow-root WP_REDIS_DATABASE 0
+
+wp config set --allow-root WP_CACHE 'true'
+
+wp config set --allow-root FS_METHOD 'direct'
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root
 
+chmod -R 755 /var/www/html/wp-content
 
 wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
