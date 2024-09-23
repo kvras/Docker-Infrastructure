f16ef0ae433437d9f3a4ca768a04698f69af2a2a
Your Name <you@example.com>
2024-09-23 01:37:05 +0000

    Fix : redis working properly

--- srcs/requirements/wordpress/wp_setup.sh
+++ srcs/requirements/wordpress/wp_setup.sh
@@ -1,16 +1,10 @@
 
-service php7.4-fpm start
-
 cd /var/www/html
 
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
-
 chmod +x /usr/local/bin/wp
-
-
-
 wp core download --path="/var/www/html" --allow-root
 
 wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb
@@ -25,16 +19,12 @@ wp config set --allow-root FS_METHOD 'direct'
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root
 
-chmod -R 777 /var/www/html/wp-content/plugins
-chown -R www-data:www-data /var/www/html/wp-content/plugins
-
-
+chmod -R 777 /var/www/html/wp-content
 
 wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
 wp plugin activate redis-cache --allow-root
 
-service php7.4-fpm stop
 
 php-fpm7.4 -F
 
